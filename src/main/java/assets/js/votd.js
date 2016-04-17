d3a6fc2caf15942b03a047762fdb374c5fbc2065
kaurm <k83.mandy@gmail.com>
2016-04-17 15:32:02 +1000

    Removing Jquery UI

--- src/main/java/assets/js/votd.js
+++ src/main/java/assets/js/votd.js
@@ -1,15 +1,20 @@
-$(document).ready(
-		function() {
-			 $('#startDate').datepicker();
-		});
+$(document).ready(function() {
+//	$('.open-startDate').click(function(event) {
+//		event.preventDefault();
+//		$('#startDate').click();
+//	});
+//	$('#startDate input').click(function(event){
+//		   $('#startDate ').datepicker();
+//		});
+//	$('#startDate').datepicker();
+});
 
-function test(){
-	
-	var availableTags = [ "ActionScript", "AppleScript", "Asp",
-			"BASIC", "C", "C++", "Clojure", "COBOL", "ColdFusion",
-			"Erlang", "Fortran", "Groovy", "Haskell", "Java",
-			"JavaScript", "Lisp", "Perl", "PHP", "Python", "Ruby",
-			"Scala", "Scheme" ];
+function test() {
+
+	var availableTags = [ "ActionScript", "AppleScript", "Asp", "BASIC", "C",
+			"C++", "Clojure", "COBOL", "ColdFusion", "Erlang", "Fortran",
+			"Groovy", "Haskell", "Java", "JavaScript", "Lisp", "Perl", "PHP",
+			"Python", "Ruby", "Scala", "Scheme" ];
 
 	$(".autocomplete").autocomplete({
 		source : availableTags
