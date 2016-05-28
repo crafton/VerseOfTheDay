048419ad712dcfb9c82665119ed2228f55ab920b
Crafton Williams <craftonjr@gmail.com>
2016-05-28 17:16:18 +1000

    Made controller fetch methods transactional to avoid bug in UnitOfWork.[#107616856]

--- src/main/java/models/theme.java
+++ src/main/java/models/theme.java
@@ -1,20 +1,21 @@
 package models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * Created by Crafton Williams on 28/05/2016.
  */
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "Theme.findAll", query = "SELECT x FROM Theme x ")
+})
 public class Theme {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Long id;
+    private Long id;
 
+    @Column(unique = true)
     private String themeName;
 
     public Theme(){}
@@ -26,4 +27,9 @@ public class Theme {
     public void setThemeName(String themeName) {
         this.themeName = themeName;
     }
+
+    public Long getId(){
+        return this.id;
+    }
+
 }
